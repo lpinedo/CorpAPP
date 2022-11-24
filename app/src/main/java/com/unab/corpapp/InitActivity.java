d0a2ea0089d561ebac4a37aa97aa1c7de56647f7
ziinsem <menger16@gmail.com>
2022-11-24 10:43:20 -0500

    inciando

--- app/src/main/java/com/unab/corpapp/InitActivity.java
+++ app/src/main/java/com/unab/corpapp/InitActivity.java
@@ -9,6 +9,6 @@ public class InitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_init);
+        setContentView(R.layout.activity_login);
     }
 }
