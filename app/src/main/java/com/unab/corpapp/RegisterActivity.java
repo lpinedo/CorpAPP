297f86ff3ca6b88d631e4aefc5e6e8b8870daa6b
lpinedo <leyder.pinedo@gmail.com>
2022-11-30 14:18:32 -0500

    formulario full

--- app/src/main/java/com/unab/corpapp/RegisterActivity.java
+++ app/src/main/java/com/unab/corpapp/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.unab.corpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,15 +11,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button btn_registerR,btn_dateId;
-    EditText name,lastname, dateB,sexo,career,email,mobile;
-    private Spinner mySpinner;
+    Button btn_registerR;
+    ImageButton btnDateIn;
+    EditText name,lastname, dateView,sexo,career,email,mobile;
+    //private Spinner mySpinner;
 
 
     @Override
@@ -26,16 +30,19 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mySpinner = (Spinner) findViewById(R.id.spinnerId);
+        dateView = findViewById(R.id.txt_InputFechaInR);
+        btnDateIn = findViewById(R.id.btn_dateId);
 
-        ArrayList<String> listS = new ArrayList<String>();
+        //mySpinner = (Spinner) findViewById(R.id  n.spinnerId);
 
-        listS.add("Masculino");
-        listS.add("Femenino");
+        //ArrayList<String> listS = new ArrayList<String>();
 
-        ArrayAdapter adp = new ArrayAdapter(RegisterActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listS);
+       // listS.add("Masculino");
+        //listS.add("Femenino");
 
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+       // ArrayAdapter adp = new ArrayAdapter(RegisterActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listS);
+
+       /* mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String elemento = (String) mySpinner.getAdapter().getItem(i);
@@ -48,13 +55,33 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        mySpinner.setAdapter(adp);
+        mySpinner.setAdapter(adp);*/
 
         btn_registerR = findViewById(R.id.btn_loginP);
         btn_registerR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(RegisterActivity.this, MenActivity.class));
+            }
+        });
+
+        btnDateIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //final Calendar c = Calendar.getInstance();
+                Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dateView.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
             }
         });
     }
